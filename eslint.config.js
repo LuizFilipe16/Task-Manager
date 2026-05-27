fb644672da46ad376b06b8cb47c9fcab833cf6d1
Luiz Filipe <filipe.oli2016@gmail.com>
2026-05-27 09:47:00 -0300

    chore: add poppins font

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
@@ -12,20 +13,34 @@ export default [
         languageOptions: {
             globals: globals.browser,
             parserOptions: {
-                ecmaFeatures: { jsx: true },
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
             },
         },
         plugins: {
+            react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
         },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
         rules: {
             ...js.configs.recommended.rules,
+            ...react.configs.recommended.rules,
             ...reactHooks.configs.recommended.rules,
+
             'react-refresh/only-export-components': [
                 'warn',
                 { allowConstantExport: true },
             ],
+
+            'react/react-in-jsx-scope': 'off',
         },
     },
 ]
